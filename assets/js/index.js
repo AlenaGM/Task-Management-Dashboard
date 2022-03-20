0da2d375a7a9e7b9f80f22e767696f62a531f57c
AlenaGM <alena.guillaume4@gmail.com>
2022-03-20 23:32:55 +0100

    upd installed npm

---
+++ assets/js/index.js
@@ -0,0 +1,52 @@
+"use strict"
+
+const moment = require('moment');
+let date = moment();
+console.log(date);
+
+const Chart = require('chart.js');
+const myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+document.querySelector('#chart').innerHTML = myChart;
+
+const datepicker = require('js-datepicker');
+const picker = datepicker('#date', {
+    onSelect: (instance, date) => {
+        console.log('Ura! Pyatnitsa!')
+    }
+})
+
+
